54b0ab3a01f03323cef261ebb54d3d593df21274
Nicolas Ondráš <32815144+fa5tkill3r@users.noreply.github.com>
2017-10-26 16:17:17 +0200

    Release
    1.0

---
+++ bot.js
@@ -0,0 +1,29 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!ping') {
+        message.reply('pong');
+    }
+
+    if (message.content === '!peto') { // peto
+        message.reply('Peto je slaby!');
+    }
+    if (message.content === '!help') { // peto
+        message.reply('GameStyle BOT\n1. !peto - Peto je slaby\n2. !ping - pong');
+    }
+    if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+            .then(connection => {
+                const dispatcher = connection.playFile('lubos.mp3');
+                message.reply('Kde je luboš?');
+            });
+    }
+});
+
+client.login('MzcyODI4MTAyMTA5NDI5Nzc2.DNJ4zg.iAWdY3Tjedi2DFNVLwBT35i1IAA');
