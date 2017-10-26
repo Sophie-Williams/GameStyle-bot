2f221d92ee273d8c9d573499217f44e4c0b8eedc
Nicolas Ondráš <32815144+fa5tkill3r@users.noreply.github.com>
2017-10-26 17:32:49 +0200

    Update beta 1.0
    Funkčná (celkom)

--- bot.js
+++ bot.js
@@ -13,19 +13,23 @@ client.on('message', message => {
 
     if (message.content === '!ping') {
         message.reply('pong');
-		console.log(`${message.author} napísal !ping`);
+		console.log(`${author.username} napísal !ping`);
     }
 
     if (message.content === '!peto') {
         message.reply('Peto je slaby!');
+		console.log(`${author.username} napísal !peto`);
     }
     if (message.content === '!help') {
         message.reply('GameStyle BOT\n1. !peto - Peto je slaby\n2. !ping - pong');
+		console.log(`${author.username} napísal !help`);
     }
     if (message.content.match(/ahoj/gi)) {
         message.reply('Ahoj ja som bot Ludvik! Som k vaším službám.');
+		console.log(`${author.username} napísal ahoj`);
     }
     if(message.content === '!lubos') {
+		console.log(`${author.username} napísal !lubos`);
         if (message.member.voiceChannel) {
             message.member.voiceChannel.join()
                 .then(connection => {
